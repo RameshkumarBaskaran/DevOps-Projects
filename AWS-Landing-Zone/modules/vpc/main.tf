4fb44c15fd86b20368f76ac642c09ddacbf0aca1
Ramesh Kumar Baskaran <rameshkumar.baskaran@gmail.com>
2026-08-04 21:41:54 +0530

    Refactor VPC and subnet resources in main.tf

--- AWS-Landing-Zone/modules/vpc/main.tf
+++ AWS-Landing-Zone/modules/vpc/main.tf
@@ -1,32 +1,24 @@
 resource "aws_vpc" "this" {
-
-  cidr_block = var.vpc_cidr
-
-  enable_dns_support = true
-
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-
-    Name = "${var.environment}-vpc"
-
+    Name        = "${var.environment}-vpc"
     Environment = var.environment
-
   }
-
 }
-resource "aws_internet_gateway" "this" {
 
+resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
 
   tags = {
     Name        = "${var.environment}-igw"
     Environment = var.environment
   }
-
 }
-resource "aws_subnet" "public_1" {
 
+resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = var.public_subnets[0]
   availability_zone       = var.availability_zones[0]
@@ -38,8 +30,8 @@ resource "aws_subnet" "public_1" {
     Tier        = "Public"
   }
 }
-resource "aws_subnet" "public_2" {
 
+resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = var.public_subnets[1]
   availability_zone       = var.availability_zones[1]
