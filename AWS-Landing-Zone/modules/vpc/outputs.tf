331b6f47e52c488a7f4be2c13c59271c55124169
Ramesh Kumar Baskaran <rameshkumar.baskaran@gmail.com>
2026-08-04 21:32:38 +0530

    Simplify output variables in outputs.tf
    Removed descriptions from output variables in outputs.tf.

--- AWS-Landing-Zone/modules/vpc/outputs.tf
+++ AWS-Landing-Zone/modules/vpc/outputs.tf
@@ -1,16 +1,12 @@
 output "vpc_id" {
-  description = "VPC ID"
-  value       = aws_vpc.this.id
+  value = aws_vpc.this.id
 }
 
 output "internet_gateway_id" {
-  description = "Internet Gateway ID"
-  value       = aws_internet_gateway.this.id
+  value = aws_internet_gateway.this.id
 }
 
 output "public_subnet_ids" {
-  description = "Public Subnet IDs"
-
   value = [
     aws_subnet.public_1.id,
     aws_subnet.public_2.id
