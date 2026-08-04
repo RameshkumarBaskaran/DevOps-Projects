a506ee2d2adcbbdbd7256999a1d966c5c7cd7ddf
Ramesh Kumar Baskaran <rameshkumar.baskaran@gmail.com>
2026-08-04 21:47:01 +0530

    Refactor VPC configuration in main.tf
    Removed redundant resources for internet gateway and public subnets.

--- AWS-Landing-Zone/modules/vpc/main.tf
+++ AWS-Landing-Zone/modules/vpc/main.tf
@@ -8,38 +8,3 @@ resource "aws_vpc" "this" {
     Environment = var.environment
   }
 }
-
-resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.this.id
-
-  tags = {
-    Name        = "${var.environment}-igw"
-    Environment = var.environment
-  }
-}
-
-resource "aws_subnet" "public_1" {
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.public_subnets[0]
-  availability_zone       = var.availability_zones[0]
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name        = "${var.environment}-public-subnet-1"
-    Environment = var.environment
-    Tier        = "Public"
-  }
-}
-
-resource "aws_subnet" "public_2" {
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.public_subnets[1]
-  availability_zone       = var.availability_zones[1]
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name        = "${var.environment}-public-subnet-2"
-    Environment = var.environment
-    Tier        = "Public"
-  }
-}
