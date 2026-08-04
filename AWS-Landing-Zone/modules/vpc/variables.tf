7bea03f22a5732b3852cdd04dd2bcd44e8fed9cb
Ramesh Kumar Baskaran <rameshkumar.baskaran@gmail.com>
2026-08-04 19:48:25 +0530

    Enhance variable definitions in variables.tf
    Added descriptions and types for VPC variables.

--- AWS-Landing-Zone/modules/vpc/variables.tf
+++ AWS-Landing-Zone/modules/vpc/variables.tf
@@ -1,3 +1,27 @@
-variable "environment" {}
+variable "environment" {
+  description = "Environment Name"
+  type        = string
+}
 
-variable "vpc_cidr" {}
+variable "vpc_cidr" {
+  description = "VPC CIDR Block"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "Availability Zones"
+
+  type = list(string)
+}
+
+variable "public_subnets" {
+  description = "Public Subnet CIDRs"
+
+  type = list(string)
+}
+
+variable "private_subnets" {
+  description = "Private Subnet CIDRs"
+
+  type = list(string)
+}
