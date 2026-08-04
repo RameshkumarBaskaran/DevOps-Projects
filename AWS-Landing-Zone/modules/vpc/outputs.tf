0777ca7bc350edd9a2344e68060ba8e5269b7a03
Ramesh Kumar Baskaran <rameshkumar.baskaran@gmail.com>
2026-08-04 20:58:35 +0530

    Refactor VPC outputs to include internet gateway and public subnets

--- AWS-Landing-Zone/modules/vpc/outputs.tf
+++ AWS-Landing-Zone/modules/vpc/outputs.tf
@@ -1,23 +1,15 @@
 output "vpc_id" {
-
   value = aws_vpc.this.id
-
 }
 
-output "public_subnet_ids" {
-
-  value = [
-    aws_subnet.public1.id,
-    aws_subnet.public2.id
-  ]
-
+output "internet_gateway_id" {
+  value = aws_internet_gateway.this.id
 }
 
-output "private_subnet_ids" {
+output "public_subnet_1_id" {
+  value = aws_subnet.public_1.id
+}
 
-  value = [
-    aws_subnet.private1.id,
-    aws_subnet.private2.id
-  ]
-
+output "public_subnet_2_id" {
+  value = aws_subnet.public_2.id
 }
