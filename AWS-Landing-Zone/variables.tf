8a530f1ee2d1eb67360bf4a1a0b1344765765fef
Ramesh Kumar Baskaran <rameshkumar.baskaran@gmail.com>
2026-08-04 19:49:18 +0530

    Refactor variables in variables.tf to include types
    Added type definitions for existing variables and included new variables for availability zones and subnets.

--- AWS-Landing-Zone/variables.tf
+++ AWS-Landing-Zone/variables.tf
@@ -1,11 +1,23 @@
 variable "aws_region" {
-  default = "ap-south-1"
+  type = string
 }
 
 variable "environment" {
-  default = "dev"
+  type = string
 }
 
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+  type = string
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
 }
