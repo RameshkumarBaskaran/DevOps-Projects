353017ab5cf1fa165986782eabe65c510ca7a093
Ramesh Kumar Baskaran <rameshkumar.baskaran@gmail.com>
2026-08-04 19:38:24 +0530

    Add VPC outputs for IDs of subnets and VPC

---
+++ AWS-Landing-Zone/modules/vpc/outputs.tf
@@ -0,0 +1,23 @@
+output "vpc_id" {
+
+  value = aws_vpc.this.id
+
+}
+
+output "public_subnet_ids" {
+
+  value = [
+    aws_subnet.public1.id,
+    aws_subnet.public2.id
+  ]
+
+}
+
+output "private_subnet_ids" {
+
+  value = [
+    aws_subnet.private1.id,
+    aws_subnet.private2.id
+  ]
+
+}
