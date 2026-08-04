ab2c129e4f94ca89c6b43b5e6bc39cdb9bd2519c
Ramesh Kumar Baskaran <rameshkumar.baskaran@gmail.com>
2026-08-04 21:49:52 +0530

    Refactor variable definitions in variables.tf

--- AWS-Landing-Zone/modules/vpc/variables.tf
+++ AWS-Landing-Zone/modules/vpc/variables.tf
@@ -1,27 +1,19 @@
 variable "environment" {
-  description = "Environment Name"
-  type        = string
+  type = string
 }
 
 variable "vpc_cidr" {
-  description = "VPC CIDR Block"
-  type        = string
+  type = string
 }
 
 variable "availability_zones" {
-  description = "Availability Zones"
-
   type = list(string)
 }
 
 variable "public_subnets" {
-  description = "Public Subnet CIDRs"
-
   type = list(string)
 }
 
 variable "private_subnets" {
-  description = "Private Subnet CIDRs"
-
   type = list(string)
 }
