05009c64f0898df915f131b13fce04140ca6663a
Ramesh Kumar Baskaran <rameshkumar.baskaran@gmail.com>
2026-08-05 10:40:03 +0530

    Add default values for aws_region and environment
    Set default values for aws_region and environment variables.

--- AWS-Landing-Zone/variables.tf
+++ AWS-Landing-Zone/variables.tf
@@ -1,23 +1,11 @@
 variable "aws_region" {
-  type = string
+
+  default = "ap-south-1"
+
 }
 
 variable "environment" {
-  type = string
-}
 
-variable "vpc_cidr" {
-  type = string
-}
+  default = "dev"
 
-variable "availability_zones" {
-  type = list(string)
-}
-
-variable "public_subnets" {
-  type = list(string)
-}
-
-variable "private_subnets" {
-  type = list(string)
 }
