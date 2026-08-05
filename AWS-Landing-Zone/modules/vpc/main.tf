c0578f6a6b6651d4773699dad88ddab2628f5609
RameshKumarBaskaran <rameshkumar.baskaran@gmail.com>
2026-08-05 16:54:34 +0530

    Sprint 2 - Add Internet Gateway module

--- AWS-Landing-Zone/modules/vpc/main.tf
+++ AWS-Landing-Zone/modules/vpc/main.tf
@@ -8,3 +8,12 @@ resource "aws_vpc" "this" {
     Environment = var.environment
   }
 }
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name        = "${var.environment}-igw"
+    Environment = var.environment
+  }
+}
--- AWS-Landing-Zone/variables.tf
+++ AWS-Landing-Zone/variables.tf
@@ -1,11 +1,41 @@
 variable "aws_region" {
-
+  type    = string
   default = "ap-south-1"
-
 }
 
 variable "environment" {
-
+  type    = string
   default = "dev"
+}
 
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  type = list(string)
+
+  default = [
+    "ap-south-1a",
+    "ap-south-1b"
+  ]
+}
+
+variable "public_subnets" {
+  type = list(string)
+
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+}
+
+variable "private_subnets" {
+  type = list(string)
+
+  default = [
+    "10.0.11.0/24",
+    "10.0.12.0/24"
+  ]
 }
