9ba0f6236b623b1bfbb0ae6f4477feb0a7eb488f
Ramesh Kumar Baskaran <rameshkumar.baskaran@gmail.com>
2026-08-05 10:42:18 +0530

    Simplify VPC module instantiation
    Removed unnecessary variables from VPC module instantiation.

--- AWS-Landing-Zone/main.tf
+++ AWS-Landing-Zone/main.tf
@@ -1,9 +1,5 @@
 module "vpc" {
+
   source = "./modules/vpc"
 
-  environment        = var.environment
-  vpc_cidr           = var.vpc_cidr
-  availability_zones = var.availability_zones
-  public_subnets     = var.public_subnets
-  private_subnets    = var.private_subnets
 }
