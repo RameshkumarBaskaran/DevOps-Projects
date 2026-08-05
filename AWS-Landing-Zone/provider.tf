9aff13b7e5a8686fb5106cf406ade12d8480861c
Ramesh Kumar Baskaran <rameshkumar.baskaran@gmail.com>
2026-08-05 10:39:34 +0530

    Reorganize tags in AWS provider configuration
    Reordered default tags in the AWS provider configuration.

--- AWS-Landing-Zone/provider.tf
+++ AWS-Landing-Zone/provider.tf
@@ -1,12 +1,17 @@
 provider "aws" {
+
   region = var.aws_region
 
   default_tags {
+
     tags = {
-      Project     = "Enterprise-Landing-Zone"
+
       Environment = var.environment
-      Owner       = "Ramesh"
+      Project     = "Enterprise-Landing-Zone"
       ManagedBy   = "Terraform"
+
     }
+
   }
+
 }
