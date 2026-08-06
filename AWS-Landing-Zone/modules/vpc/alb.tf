34a1e033ca9e5b1ecd8df172ae841a5c08dde75d
RameshKumarBaskaran <rameshkumar.baskaran@gmail.com>
2026-08-06 12:14:45 +0530

    Sprint 15 - Add Application Load Balancer Target Group

--- AWS-Landing-Zone/modules/vpc/alb.tf
+++ AWS-Landing-Zone/modules/vpc/alb.tf
@@ -53,3 +53,42 @@ resource "aws_lb" "this" {
     Environment = var.environment
   }
 }
+resource "aws_lb_target_group" "app" {
+
+  name = "${var.environment}-app-tg"
+
+  port = 80
+
+  protocol = "HTTP"
+
+  vpc_id = aws_vpc.this.id
+
+  health_check {
+
+    enabled = true
+
+    path = "/"
+
+    protocol = "HTTP"
+
+    matcher = "200"
+
+    interval = 30
+
+    timeout = 5
+
+    healthy_threshold = 3
+
+    unhealthy_threshold = 3
+
+  }
+
+  tags = {
+
+    Name = "${var.environment}-app-tg"
+
+    Environment = var.environment
+
+  }
+
+}
--- AWS-Landing-Zone/modules/vpc/outputs.tf
+++ AWS-Landing-Zone/modules/vpc/outputs.tf
@@ -50,3 +50,8 @@ output "alb_arn" {
 output "alb_dns_name" {
   value = aws_lb.this.dns_name
 }
+output "target_group_arn" {
+
+  value = aws_lb_target_group.app.arn
+
+}
