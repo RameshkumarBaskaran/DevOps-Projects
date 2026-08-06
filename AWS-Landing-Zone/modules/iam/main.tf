fca92fd8aae70b5d855189bdd57929b282ffb59b
RameshKumarBaskaran <rameshkumar.baskaran@gmail.com>
2026-08-06 20:00:31 +0530

    Sprint 34 - Add Custom IAM Policy

--- AWS-Landing-Zone/modules/iam/main.tf
+++ AWS-Landing-Zone/modules/iam/main.tf
@@ -51,3 +51,57 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   role = aws_iam_role.ec2_role.name
 
 }
+resource "aws_iam_policy" "ec2_custom_policy" {
+
+  name = "${var.environment}-ec2-custom-policy"
+
+  description = "Custom policy for EC2 instances"
+
+  policy = jsonencode({
+
+    Version = "2012-10-17"
+
+    Statement = [
+
+      {
+
+        Effect = "Allow"
+
+        Action = [
+
+          "ec2:DescribeInstances",
+
+          "ec2:DescribeTags"
+
+        ]
+
+        Resource = "*"
+
+      },
+
+      {
+
+        Effect = "Allow"
+
+        Action = [
+
+          "cloudwatch:PutMetricData"
+
+        ]
+
+        Resource = "*"
+
+      }
+
+    ]
+
+  })
+
+}
+resource "aws_iam_role_policy_attachment" "custom_policy" {
+
+  role = aws_iam_role.ec2_role.name
+
+  policy_arn = aws_iam_policy.ec2_custom_policy.arn
+
+}
