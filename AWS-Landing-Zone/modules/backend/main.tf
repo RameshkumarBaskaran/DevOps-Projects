39fe68b2ef35dc58a5abb65a84508b67985fd889
RameshKumarBaskaran <rameshkumar.baskaran@gmail.com>
2026-08-06 19:17:36 +0530

    Sprint 29 - Add DynamoDB State Lock Table

--- AWS-Landing-Zone/modules/backend/main.tf
+++ AWS-Landing-Zone/modules/backend/main.tf
@@ -20,3 +20,28 @@ resource "aws_s3_bucket_versioning" "terraform_state" {
   }
 
 }
+resource "aws_dynamodb_table" "terraform_lock" {
+
+  name = "${var.environment}-terraform-lock"
+
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "LockID"
+
+  attribute {
+
+    name = "LockID"
+
+    type = "S"
+
+  }
+
+  tags = {
+
+    Name = "${var.environment}-terraform-lock"
+
+    Environment = var.environment
+
+  }
+
+}
